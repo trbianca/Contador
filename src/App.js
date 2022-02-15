025b1f8781996a21ff0a6378213f73ec659e8346
trbianca <trbianca@outlook.com>
2022-02-15 16:28:53 -0300

    Contador básico finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cont: 0
+  };
+
+  adicionar = () => {
+    if (this.state.cont < 10) {
+      this.setState({
+        cont: this.state.cont + 1
+      });
+    }
+  };
+
+  diminuir = () => {
+    if (this.state.cont > 0) {
+      this.setState({
+        cont: this.state.cont - 1
+      });
+    }
+  };
+
+  zerar = () => {
+    this.setState({
+      cont: 0
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="container-main">
+          <div className="container-main-name">
+            <h1>Contador</h1>
+          </div>
+
+          <div className="larger-circle">
+            <div className="smaller-circle">
+              <h3>{this.state.cont}</h3>
+            </div>
+          </div>
+
+          <div className="increase-decrease-button">
+            <button onClick={this.adicionar}>+</button>
+            <button onClick={this.diminuir}>-</button>
+          </div>
+
+          <div className="reset-button">
+            <button onClick={this.zerar}>Z</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
